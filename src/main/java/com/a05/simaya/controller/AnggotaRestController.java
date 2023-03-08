24badbc460ea8273c1ccd0816a4cda80f98537b7
charlespramudana <charles.pramudana@ui.ac.id>
2023-03-08 15:07:51 +0700

    feat: add controller and view for daftar anggota

---
+++ src/main/java/com/a05/simaya/controller/AnggotaRestController.java
@@ -0,0 +1,14 @@
+package com.a05.simaya.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AnggotaRestController {
+
+}
